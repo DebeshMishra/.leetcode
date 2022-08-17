64ac00fb9e0b3e7ec577ccc0fd3bbf39ed8d984b
DebeshMishra <98782809+DebeshMishra@users.noreply.github.com>
2022-08-17 14:33:14 -0700

    daily

---
+++ 804.unique-morse-code-words.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode id=804 lang=java
+ *
+ * [804] Unique Morse Code Words
+ */
+
+// @lc code=start
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morseCode = new String[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                "...-", ".--", "-..-", "-.--", "--.." };
+
+        Set<String> uniqueCodes = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (Character c : word.toCharArray())
+                sb.append(morseCode[c - 'a']);
+
+            uniqueCodes.add(sb.toString());
+        }
+
+        return uniqueCodes.size();
+    }
+}
+// @lc code=end
