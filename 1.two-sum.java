06034c7b74b36c786be1bad76fc3fbd3ec3e0ac2
Debesh Mishra <98782809+DebeshMishra@users.noreply.github.com>
2023-06-25 21:18:55 -0700

    updates

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -8,40 +8,47 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int[] arr = Arrays.copyOf(nums, nums.length);
+        //hashmap
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
 
-        Arrays.sort(nums);
-        int i = 0, j = nums.length - 1;
-        while (i < j) {
-            if (nums[i] + nums[j] == target)
-                break;
-            else if (nums[i] + nums[j] < target)
-                i++;
-            else
-                j--;
+        for(int i=0; i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[] {i,map.get(target-nums[i])};
+            }
+            map.put(nums[i],i);
         }
+        return null;
 
-        int idx1 = -1;
-        int idx2 = -1;
-        for (int k = 0; k < arr.length; k++) {
-            if (idx1 == -1 && arr[k] == nums[i])
-                idx1 = k;
-            else if (idx2 == -1 && arr[k] == nums[j])
-                idx2 = k;
-        }
+        //sliding window
+        // Arrays.sort(nums);
+        // int i = 0, j = nums.length - 1;
+        // while (i < j) {
+        //     if (nums[i] + nums[j] == target)
+        //         break;
+        //     else if (nums[i] + nums[j] < target)
+        //         i++;
+        //     else
+        //         j--;
+        // }
+        // int idx1 = -1;
+        // int idx2 = -1;
+        // for (int k = 0; k < arr.length; k++) {
+        //     if (idx1 == -1 && arr[k] == nums[i])
+        //         idx1 = k;
+        //     else if (idx2 == -1 && arr[k] == nums[j])
+        //         idx2 = k;
+        // }
+        // return new int[] { idx1, idx2 };
 
-        return new int[] { idx1, idx2 };
-
-        /*
-         * for(int i=0;i<nums.length;i++){
-         * for(int j=1;j<nums.length;j++){
-         * if(nums[i]+nums[j]==target && i!=j){
-         * return new int[] {i,j};
-         * }
-         * }
-         * }
-         * throw new IllegalArgumentException("aa");
-         */
+        //brute-force approach
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=1;j<nums.length;j++){
+        //         if(nums[i]+nums[j]==target && i!=j){
+        //             return new int[] {i,j};
+        //         }
+        //     }
+        // }
+        // throw new IllegalArgumentException("aa");
     }
 }
 // @lc code=end
