79dfc4521b9dc1b666c296315c1f609858e77971
Debesh Mishra <98782809+DebeshMishra@users.noreply.github.com>
2023-06-26 23:11:10 -0700

    updated blind75

--- 217.contains-duplicate.java
+++ 217.contains-duplicate.java
@@ -8,24 +8,33 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        boolean flag = false;
+        HashSet<Integer> numsSet = new HashSet<Integer>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.get(nums[i]) != null)
-                map.put(nums[i], map.get(nums[i]) + 1);
-            else
-                map.put(nums[i], 1);
-        }
-
-        for (Integer i : map.values()) {
-            if (i > 1) {
-                flag = true;
-                break;
+        for(int i=0;i<nums.length;i++){
+            if(numsSet.contains(nums[i])){
+                return true;
             }
+            numsSet.add(nums[i]);
         }
 
-        return flag;
+        return false;
+
+        //old messy approach
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // boolean flag = false;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.get(nums[i]) != null)
+        //         map.put(nums[i], map.get(nums[i]) + 1);
+        //     else
+        //         map.put(nums[i], 1);
+        // }
+        // for (Integer i : map.values()) {
+        //     if (i > 1) {
+        //         flag = true;
+        //         break;
+        //     }
+        // }
+        // return flag;
     }
 }
 // @lc code=end
