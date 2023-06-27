ba82d2d3fa622c3643853d7dbecef1c809fb07e5
Debesh Mishra <98782809+DebeshMishra@users.noreply.github.com>
2023-06-26 23:00:32 -0700

    updated blind 75

--- 242.valid-anagram.java
+++ 242.valid-anagram.java
@@ -7,20 +7,45 @@
 // @lc code=start
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        int[] counter = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            counter[s.charAt(i) - 'a']++;
-            counter[t.charAt(i) - 'a']--;
-        }
-        for (int count : counter) {
-            if (count != 0) {
-                return false;
+        HashMap<Character,Integer> sMap = new HashMap<Character,Integer>();
+        HashMap<Character,Integer> tMap = new HashMap<Character,Integer>();
+
+        if(s.length()==t.length()){
+            for(int i=0;i<s.length();i++){
+                if(sMap.containsKey(s.charAt(i)))
+                    sMap.put(s.charAt(i),sMap.get(s.charAt(i))+1);
+                else
+                    sMap.put(s.charAt(i),1);
             }
+    
+            for(int j=0;j<t.length();j++){
+                if(tMap.containsKey(t.charAt(j)))
+                    tMap.put(t.charAt(j),tMap.get(t.charAt(j))+1);
+                else
+                    tMap.put(t.charAt(j),1);
+            }
+    
+            return sMap.equals(tMap);
         }
-        return true;
+        
+        else
+            return false;
+
+        //different approach
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
+        // int[] counter = new int[26];
+        // for (int i = 0; i < s.length(); i++) {
+        //     counter[s.charAt(i) - 'a']++;
+        //     counter[t.charAt(i) - 'a']--;
+        // }
+        // for (int count : counter) {
+        //     if (count != 0) {
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
 // @lc code=end
