c4dbd56db5b233fc974abe7de310ad7ae7d6883a
Debesh Mishra <98782809+DebeshMishra@users.noreply.github.com>
2023-06-27 15:19:37 -0700

    blind 75

--- 49.group-anagrams.java
+++ 49.group-anagrams.java
@@ -8,26 +8,36 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        HashMap<String, List<String>> map = new HashMap();
+        HashMap<String, List<String>> map = new HashMap<>();
 
-        for (int i = 0; i < strs.length; i++) {
-            String s = strs[i];
-
-            char[] temp = s.toCharArray();
-            Arrays.sort(temp);
-            String s1 = new String(temp);
-
-            if (map.get(s1) != null)
-                map.get(s1).add(s);
-
-            else {
-                map.put(s1, new ArrayList());
-                map.get(s1).add(s);
+        for (int i=0; i<strs.length; i++) {
+            int[] count = new int[26];
+            for (char c : strs[i].toCharArray()) {
+                count[c-'a']++;
             }
-
+            String key = new String(Arrays.toString(count));
+            map.computeIfAbsent(key, k -> new ArrayList<>());
+            map.get(key).add(strs[i]);
         }
 
         return new ArrayList(map.values());
+
+
+        //using sorting
+        // HashMap<String, List<String>> map = new HashMap();
+        // for (int i = 0; i < strs.length; i++) {
+        //     String s = strs[i];
+        //     char[] temp = s.toCharArray();
+        //     Arrays.sort(temp);
+        //     String s1 = new String(temp);
+        //     if (map.get(s1) != null)
+        //         map.get(s1).add(s);
+        //     else {
+        //         map.put(s1, new ArrayList());
+        //         map.get(s1).add(s);
+        //     }
+        // }
+        // return new ArrayList(map.values());
     }
 }
 // @lc code=end
